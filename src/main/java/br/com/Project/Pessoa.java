ce0801edb7ca77c7b65ba9a6929aa1fcd570cdf3
Tobias Cardoso <tobiascv@gmail.com>
2023-12-11 22:08:26 -0300

    Incio projeto

---
+++ src/main/java/br/com/Project/Pessoa.java
@@ -0,0 +1,12 @@
+package br.com.Project;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Pessoa {
+   private String nome;
+   private int idade;
+
+}
---
+++ src/main/java/br/com/Project/main.java
@@ -0,0 +1,7 @@
+package br.com.Project;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}

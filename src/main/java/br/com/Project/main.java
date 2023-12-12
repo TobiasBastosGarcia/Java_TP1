f02ff865d3246e4f6196a4340e4188b6278aae42
Tobias Cardoso <tobiascv@gmail.com>
2023-12-12 10:49:39 -0300

    Adicionou classes nos packages

--- src/main/java/br/com/Project/main.java
+++ src/main/java/br/com/Project/main.java
@@ -1,7 +1,26 @@
 package br.com.Project;
+import java.util.Scanner;
+import br.com.Project.Pack1.Somar;
+import br.com.Project.Pack2.Subtrair;
+import br.com.Project.Pack3.Multiplicar;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Somar som = new Somar();
+        int resultado1 = som.soma(5,6);
+        System.out.println(resultado1);
+        System.out.println();
+
+        Subtrair sub = new Subtrair();
+        int resultado2 = sub.subitrai(7,16);
+        if (resultado2<0){
+            System.out.println("O resultado deu negativo!");
+        }
+        System.out.println(resultado2);
+        System.out.println();
+
+        Multiplicar mult = new Multiplicar();
+        int resultado3 = mult.multiplica(5,6);
+        System.out.println(resultado3);
     }
 }
